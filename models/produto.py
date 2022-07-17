a6f1488885554a2f6b6aa51f3d87b35d218a58cd
davilos <daviloscostagg@hotmail.com>
2022-07-17 14:00:08 -0300

    Produto corrigido com mypy

--- models/produto.py
+++ models/produto.py
@@ -5,23 +5,23 @@ class Produto:
 
     contador: int = 1
 
-    def __init__(self: object, nome: str, preco: float) -> None:
+    def __init__(self, nome: str, preco: float) -> None:
         self.__codigo: int = Produto.contador
         self.__nome: str = nome
         self.__preco: float = preco
         Produto.contador += 1
 
     @property
-    def codigo(self: object) -> int:
+    def codigo(self) -> int:
         return self.__codigo
 
     @property
-    def nome(self: object) -> str:
+    def nome(self) -> str:
         return self.__nome
 
     @property
-    def preco(self: object) -> float:
+    def preco(self) -> float:
         return self.__preco
 
-    def __str__(self: object) -> str:
+    def __str__(self) -> str:
         return f'Código: {self.codigo}\nNome: {self.nome}\nPreço: {formata_float_str_moeda(self.preco)}'
